42a143889c7abfcc8d5df37be174ead8af4a8d8e
CaelDegnian <cael.degnian@hotmail.com>
2021-06-22 22:00:28 +1000

    Added project list

--- src/UXUI.js
+++ src/UXUI.js
@@ -1,7 +1,21 @@
 import React from "react";
+import projects from "./data/projects.json";
 
 function UXUI() {
-  return <div>Hi</div>;
+  return (
+    <div>
+      <div>List of UI / UX projects</div>
+      <div>
+        {projects.map((project) => {
+          return (
+            <div>
+              {project.name}: {project.title}
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
 
 export default UXUI;
