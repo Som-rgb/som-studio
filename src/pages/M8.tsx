a05f47d4f939682c09d411b380ea9d65a150dcd1
som-rgb <sommerdeo@hotmail.com>
2022-03-27 21:47:55 +1000

    updates

--- src/pages/M8.tsx
+++ src/pages/M8.tsx
@@ -10,9 +10,8 @@ const M8 = () => {
               <div className="project-text-container">
                 <h2>M8</h2>
                 <p>
-                  A UX/UI Case study on a first in the retail industry, offering
-                  a streamlined experience for capturing monthly sales data
-                  between Tenants and Property Managers.
+                  M8 modernises and automates the sales data capture process for
+                  a portfolio of assets for a national retailer
                 </p>
               </div>
             </Col>
@@ -22,6 +21,10 @@ const M8 = () => {
                 className="iframe"
                 src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Fproto%2FKRxlBAAl7vtUs4JvSKhOpm%2FM8%3Fpage-id%3D0%253A1%26node-id%3D2%253A20%26viewport%3D331%252C48%252C0.57%26scaling%3Dcontain%26starting-point-node-id%3D2%253A20%26hide-ui%3D1"
               ></iframe>
+              <h6 className="figma-container">
+                This is just an embedded Figma file, scroll right on the frame!
+                →
+              </h6>
             </Col>
             <Col sm={2} md={2}></Col>
           </Row>
