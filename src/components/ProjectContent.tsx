2fe4a2249da3429d7b17f50a564c76c0160f484a
som-rgb <sommerdeo@hotmail.com>
2022-05-02 22:23:22 +1000

    yes

--- src/components/ProjectContent.tsx
+++ src/components/ProjectContent.tsx
@@ -19,17 +19,39 @@ const ProjectContent: React.FC<Params> = (projectFilter) => {
       {projectList.map((project, key) => {
         return (
           <>
-            <Row key={project.key} className={styles["project-container"]}>
+            <Row key={project.key} className={styles["top-container"]}>
               <Col md={1}></Col>
               <Col md={10}>
                 <Row>
                   <Col md={4}>
                     <h2>{project.title}</h2>
+
+                    <Row>
+                      <Col
+                        md={2}
+                        className={styles["project-color"]}
+                        style={{ backgroundColor: project.primary }}
+                      ></Col>
+                      <Col
+                        md={2}
+                        className={styles["project-color"]}
+                        style={{ backgroundColor: project.secondary }}
+                      ></Col>
+                      <Col
+                        md={2}
+                        className={styles["project-color"]}
+                        style={{ backgroundColor: project.teritaryOne }}
+                      ></Col>
+                      <Col
+                        md={2}
+                        className={styles["project-color"]}
+                        style={{ backgroundColor: project.teritaryTwo }}
+                      ></Col>
+                      <Col md={4}></Col>
+                    </Row>
+
                     <h4>The Problem</h4>
                     <p>{project.blurb}</p>
-                    <Link to={{ pathname: project.url }}>
-                      <Button>View Case Study</Button>
-                    </Link>
                   </Col>
                   <Col md={2}></Col>
                   <Col md={6}>
@@ -42,6 +64,14 @@ const ProjectContent: React.FC<Params> = (projectFilter) => {
                 </Row>
               </Col>
               <Col md={1}></Col>
+            </Row>
+
+            <Row></Row>
+
+            <Row>
+              <Link to={{ pathname: project.url }}>
+                <Button>View Case Study</Button>
+              </Link>
             </Row>
           </>
         );
